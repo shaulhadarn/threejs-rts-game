b8fba09f1f08f1180e81ef50332ea473504362a6
shaulhadarn <53553702+shaulhadarn@users.noreply.github.com>
2026-02-14 09:30:25 +0200

    Initial commit: Three.js RTS game with ECS architecture, unit selection and movement

---
+++ src/ecs/Component.ts
@@ -0,0 +1,41 @@
+/**
+ * Component Types Enum
+ * Defines all available component types in the ECS
+ */
+export enum ComponentType {
+  TRANSFORM = 'transform',
+  RENDERABLE = 'renderable',
+  SELECTABLE = 'selectable',
+  MOVEMENT = 'movement',
+  HEALTH = 'health',
+  UNIT = 'unit',
+  RESOURCE = 'resource',
+  BUILDING = 'building',
+}
+
+/**
+ * Base Component Interface
+ * All components must implement this interface
+ */
+export interface Component {
+  type: ComponentType;
+  enabled: boolean;
+}
+
+/**
+ * Base Component class with common functionality
+ */
+export abstract class BaseComponent implements Component {
+  abstract type: ComponentType;
+  enabled: boolean = true;
+
+  constructor() {}
+
+  enable(): void {
+    this.enabled = true;
+  }
+
+  disable(): void {
+    this.enabled = false;
+  }
+}
